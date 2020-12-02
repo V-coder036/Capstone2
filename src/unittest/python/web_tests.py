d6292707796eeaea9d4e585c90da61963e050518
V-coder036 <vikrantshetty036@gmail.com>
2020-12-02 14:28:03 +0530

    hello

--- src/unittest/python/web_tests.py
+++ src/unittest/python/web_tests.py
@@ -1,12 +1,17 @@
-#import unittest
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-#driver = webdriver.Firefox(executable_path = r'src/main/python/driver/geckodriver.exe')
-#driver.get('http://localhost:8000')
-#sbutton = driver.find_element_by_class_name("testbtn")
-#sbutton.click()
-#river.find_element_by_class_name("testbtn1").send_keys(os.getcwd()+"/Un.png")
-#driver.find_element_by_class_name("testbtn2").click()
-#z=driver.find_element_by_class_name("btn4").text
-#print(z)
-#self.assertEquals(z,"Tuberculosis")
+# import unittest
+# from selenium import webdriver
+# import os
+# from selenium.webdriver.common.keys import Keys
+# driver = webdriver.Firefox(executable_path = r'../../main/python/driver/geckodriver.exe')
+# driver.get('http://localhost:8000')
+# sbutton = driver.find_element_by_class_name("testbtn")
+# sbutton.click()
+# driver.find_element_by_class_name("testbtn1").send_keys(os.getcwd()+"\\Un.png")
+# driver.find_element_by_class_name("testbtn2").click()
+# import time
+# time.sleep(90)
+# z=driver.find_element_by_class_name("btn4").text
+# print(z)
+# class webTest(unittest.TestCase):
+#     def test_get(self):
+#         self.assertEquals(self.z,"Tuberculosis")
